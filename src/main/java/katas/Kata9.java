b7ab060f9354ecf763dbb1fb421bf49f62110042
unknown <sbarrera.96@hotmail.com>
2022-07-08 00:09:32 -0500

    Kata 9

--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -10,6 +10,7 @@ import util.DataUtil;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -19,7 +20,23 @@ import java.util.Map;
 public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
+        List<Map> videoInfo = movieLists.stream()
+                .map(movie -> movie.getVideos())
+                .flatMap(a -> a.stream().reduce((b, c) -> b.getUri().length() < c.getUri().length() ? b : c)
+                        .stream().map(e -> ImmutableMap.of("id", e.getId(),
+                                "title", e.getTitle(),
+                                "middle time", e.getInterestingMoments().stream()
+                                        .reduce((d, f) -> d.getType() == "Middle" ? d : f),
+                                "boxart", e.getBoxarts(),
+                                "url", e.getUri())))
+                .collect(Collectors.toList());
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+
+//        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        return videoInfo;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(execute());
     }
 }
